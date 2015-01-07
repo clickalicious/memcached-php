3afbfd03cfa440204561da5d9e4d00cf0240a998
Benjamin Carl <opensource@clickalicious.de>
2015-01-07 01:13:59 +0100

    Small fix

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -20,7 +20,7 @@ if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages > /dev/null 2>&1
 
   echo -e "Done magic with coverage\n"
 fi
