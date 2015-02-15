10290c213347bd9396fb31e56e5a1700ae4fd4ed
Benjamin Carl <opensource@clickalicious.de>
2015-02-15 01:58:40 +0100

    Util cleanup and refactoring // Util tests

--- lib/Clickalicious/Memcached/Util.php
+++ lib/Clickalicious/Memcached/Util.php
@@ -80,112 +80,145 @@ namespace Clickalicious\Memcached;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @copyright Copyright (c) 2013 Ben Ramsey <http://benramsey.com>
+ * @author Ben Ramsey <http://benramsey.com>
+ * @copyright 2013 Ben Ramsey
  * @license http://opensource.org/licenses/MIT MIT
  * @codeCoverageIgnore
  */
-if (!function_exists('array_column')) {
+if (false === function_exists('array_column')) {
 
     /**
-     * Returns the values from a single column of the input array, identified by
-     * the $columnKey.
+     * Returns the values from a single column of the input array, identified by the $columnKey.
      *
-     * Optionally, you may provide an $indexKey to index the values in the returned
-     * array by the values from the $indexKey column in the input array.
+     * Optionally, you may provide an $indexKey to index the values in the returned array by the values from the
+     * $indexKey column in the input array.
      *
-     * @param array $input A multi-dimensional array (record set) from which to pull
-     *                     a column of values.
-     * @param mixed $columnKey The column of values to return. This value may be the
-     *                         integer key of the column you wish to retrieve, or it
-     *                         may be the string key name for an associative array.
-     * @param mixed $indexKey (Optional.) The column to use as the index/keys for
-     *                        the returned array. This value may be the integer key
-     *                        of the column, or it may be the string key name.
+     * @param array $input      A multi-dimensional array (record set) from which to pull a column of values.
+     * @param mixed $column_key The column of values to return. This value may be the integer key of the column you
+     *                          wish to retrieve, or it may be the string key name for an associative array.
+     * @param mixed $index_key  (Optional.) The column to use as the index/keys for the returned array. This value
+     *                          may be the integer key of the column, or it may be the string key name.
      * @return array
      * @codeCoverageIgnore
      */
-    function array_column($input = null, $columnKey = null, $indexKey = null)
+    function array_column(array $input, $column_key, $index_key = null)
     {
-        // Using func_get_args() in order to check for proper number of
-        // parameters and trigger errors exactly as the built-in array_column()
-        // does in PHP 5.5.
-        $argc = func_num_args();
-        $params = func_get_args();
+        /**
+         * Proxy call to array_column_emulation(): makes it a bit cleaner and testing on all systems possible
+         */
+        return array_column_emulation($input, $column_key, $index_key);
+    }
+}
 
-        if ($argc < 2) {
-            trigger_error("array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING);
-            return null;
+/**
+ * Returns the values from a single column of the input array, identified by the $columnKey.
+ *
+ * Optionally, you may provide an $indexKey to index the values in the returned array by the values from the
+ * $indexKey column in the input array.
+ *
+ * @param array $input      A multi-dimensional array (record set) from which to pull a column of values.
+ * @param mixed $column_key The column of values to return. This value may be the integer key of the column you
+ *                          wish to retrieve, or it may be the string key name for an associative array.
+ * @param mixed $index_key  (Optional.) The column to use as the index/keys for the returned array. This value
+ *                          may be the integer key of the column, or it may be the string key name.
+ * @return array
+ */
+function array_column_emulation(array $input, $column_key, $index_key = null)
+{
+    // Check for ...
+    if (!is_int($column_key)    &&
+        !is_float($column_key)  &&
+        !is_string($column_key) &&
+        $column_key !== null    &&
+        !(
+            is_object($column_key) && method_exists($column_key, '__toString')
+        )
+    ) {
+        return !trigger_error(
+            'array_column(): The column key should be either a string or an integer',
+            E_USER_WARNING
+        );
+    }
+
+    // Check for ...
+    if (null !== $index_key     &&
+        !is_int($index_key)     &&
+        !is_float($index_key)   &&
+        !is_string($index_key)  &&
+        !(
+            is_object($index_key) && method_exists($index_key, '__toString')
+        )
+    ) {
+        return !trigger_error(
+            'array_column(): The index key should be either a string or an integer',
+            E_USER_WARNING
+        );
+    }
+
+    // Check for passed index key
+    if (null !== $index_key) {
+        if (is_float($index_key) || is_int($index_key)) {
+            $index_key = (int)$index_key;
+        } else {
+            $index_key = (string)$index_key;
+        }
+    }
+
+    $result = array();
+
+    foreach ($input as $row) {
+        $key    = null;
+        $keySet = false;
+
+        if (null !== $index_key && true === is_array($row) && true === array_key_exists($index_key, $row)) {
+            $keySet = true;
+            $key    = (string)$row[$index_key];
         }
 
-        if (!is_array($params[0])) {
-            trigger_error(
-                'array_column() expects parameter 1 to be array, ' . gettype($params[0]) . ' given', E_USER_WARNING
-            );
-            return null;
+        if (true === is_array($row) && true === array_key_exists($column_key, $row)) {
+            $value = $row[$column_key];
+        } else {
+            $value = $row;
         }
 
-        if (!is_int($params[1])
-            && !is_float($params[1])
-            && !is_string($params[1])
-            && $params[1] !== null
-            && !(is_object($params[1]) && method_exists($params[1], '__toString'))
-        ) {
-            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
-            return false;
+        if ($keySet === true) {
+            $result[$key] = $value;
+        } else {
+            $result[]     = $value;
         }
+    }
 
-        if (isset($params[2])
-            && !is_int($params[2])
-            && !is_float($params[2])
-            && !is_string($params[2])
-            && !(is_object($params[2]) && method_exists($params[2], '__toString'))
-        ) {
-            trigger_error('array_column(): The index key should be either a string or an integer', E_USER_WARNING);
-            return false;
-        }
+    return $result;
+}
 
-        $paramsInput = $params[0];
-        $paramsColumnKey = ($params[1] !== null) ? (string) $params[1] : null;
+/**
+ * Alternative implementation of boolval() for PHP releases < 5.5.
+ *
+ * @param mixed $boolean The potential boolean
+ *
+ * @author Benjamin Carl <opensource@clickalicious.de>
+ * @return bool TRUE || FALSE depending on input.
+ * @access public
+ * @codeCoverageIgnore
+ */
+if (false === function_exists('boolval')) {
 
-        $paramsIndexKey = null;
-        if (isset($params[2])) {
-            if (is_float($params[2]) || is_int($params[2])) {
-                $paramsIndexKey = (int) $params[2];
-            } else {
-                $paramsIndexKey = (string) $params[2];
-            }
-        }
-
-        $resultArray = array();
-
-        foreach ($paramsInput as $row) {
-
-            $key = $value = null;
-            $keySet = $valueSet = false;
-
-            if ($paramsIndexKey !== null && is_array($row) && array_key_exists($paramsIndexKey, $row)) {
-                $keySet = true;
-                $key = (string) $row[$paramsIndexKey];
-            }
-
-            if ($paramsColumnKey === null) {
-                $valueSet = true;
-                $value = $row;
-            } elseif (is_array($row) && array_key_exists($paramsColumnKey, $row)) {
-                $valueSet = true;
-                $value = $row[$paramsColumnKey];
-            }
-
-            if ($valueSet) {
-                if ($keySet) {
-                    $resultArray[$key] = $value;
-                } else {
-                    $resultArray[] = $value;
-                }
-            }
-        }
-
-        return $resultArray;
+    /**
+     * Alternative implementation of boolval() for PHP releases < 5.5.
+     *
+     * @param mixed $boolean The potential boolean
+     *
+     * @author Benjamin Carl <opensource@clickalicious.de>
+     * @return bool TRUE || FALSE depending on input.
+     * @access public
+     * @codeCoverageIgnore
+     */
+    function boolval($boolean)
+    {
+        /**
+         * Proxy call to boolval_emulation()
+         */
+        return boolval_emulation($boolean);
     }
 }
 
@@ -198,19 +231,16 @@ if (!function_exists('array_column')) {
  * @return bool TRUE || FALSE depending on input.
  * @access public
  */
-if (!function_exists('boolval')) {
-
-    function boolval($boolean)
-    {
-        if (is_string($boolean) == true) {
-            $boolean = strtoupper($boolean);
-        }
-
-        if (in_array($boolean, array(true, 1, 'TRUE', 'YES', 'Y', 'ON', '1'), true) === true) {
-            return true;
-        }
-
-        // at least let PHP decide :)
-        return $boolean ? true : false;
+function boolval_emulation($boolean)
+{
+    if (is_string($boolean) === true) {
+        $boolean = strtoupper($boolean);
     }
+
+    if (true === in_array($boolean, array(true, 1, 'TRUE', 'YES', 'Y', 'ON', '1'), true)) {
+        return true;
+    }
+
+    // At least let PHP decide :)
+    return $boolean ? true : false;
 }
